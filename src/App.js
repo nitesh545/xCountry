79cb2b5fa3847cc701089888a416ab610c58fab3
nitecse <hitman12457868@gmail.com>
2024-09-09 15:24:14 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Grid2 } from '@mui/material';
 
 function App() {
+  const [flags, setFlags] = useState([]);
+  const fetchFlags = () => {
+    axios.get("https://xcountries-backend.azurewebsites.net/all").then((res) => { setFlags(res.data); }).catch((err) => { console.log(err); })
+  }
+  useEffect(() => { fetchFlags(); }, []);
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      {/* <header className="App-header">
+      </header> */}
+      <Grid2 container spacing={2}>
+        {
+          flags.map((flag) => {
+            return (
+              <Grid2 size={2} sx={{ mt: 1, p: 1 }}>
+                <img src={flag.flag} alt={flag.name}></img>
+                <h4>{flag.name}</h4>
+              </Grid2>
+            );
+          })
+        }
+      </Grid2>
     </div>
   );
 }
