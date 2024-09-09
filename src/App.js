a1d2bcaa9372416e144a5c0c97e1aa4d1d7d6422
nitecse <hitman12457868@gmail.com>
2024-09-09 16:18:17 +0530

    app updated 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     fetch("https://xcountries-backend.azurewebsites.net/all")
       .then((res) => res.json())
       .then((data) => setFlags(data))
-      .catch((error) => console.log(`Error fetching data: ${error}`))
+      .catch((error) => console.log("Error fetching data:", error))
   }
   useEffect(() => { fetchFlags(); }, []);
   return (
